9151d78ddec88abc3a9f3b5c5269ce080ece1174
LitQoo <litqoo@gmail.com>
2017-07-09 17:55:28 +0900

    VectorVariable 추가

---
+++ Editor/Variable/VectorVariableTest.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+using BicDB.Container;
+using BicDB;
+using System;
+using BicDB.Utility;
+
+namespace BicDB.Variable
+{
+	public class VectorVariableTest 
+	{
+		[Test]
+		public void CreateTest(){
+			VectorVariable _var = new VectorVariable (0, 0);
+			Assert.AreEqual (_var.X.AsFloat, 0);
+			Assert.AreEqual (_var.Y.AsFloat, 0);
+		}
+
+		[Test]
+		public void AsVactorOutTest(){
+			VectorVariable _var = new VectorVariable (99, -77.2f);
+			Assert.AreEqual (_var.AsVector.x, 99);
+			Assert.AreEqual (_var.AsVector.y, -77.2f);
+		}
+
+		[Test]
+		public void createByStringTest(){
+			VectorVariable _var = new VectorVariable (99, -100);
+			string _json = "{\"x\" : 1, \"y\":-10}";
+			int _counter = 0;
+			_var.BuildVariable(ref _json, ref _counter, JsonConvertor.GetInstance());
+
+			Assert.AreEqual (_var.X.AsFloat, 1);
+			Assert.AreEqual (_var.Y.AsFloat, -10);
+		}
+
+		[Test]
+		public void buildStringTest(){
+			VectorVariable _var = new VectorVariable (99, -100);
+			string _json = "";
+			_var.BuildFormattedString(ref _json, JsonConvertor.GetInstance());
+
+			Assert.AreEqual (_json, "{\"x\":99,\"y\":-100}");
+		}
+	}
+}
---
+++ Variable/VectorVariable.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using BicDB;
+using System.Linq;
+using System.Collections.Generic;
+using BicDB.Utility;
+using BicDB.Container;
+using UnityEngine;
+
+namespace BicDB.Variable
+{
+	public class VectorVariable : DictionaryContainer<FloatVariable>
+	{
+		#region Event
+		public event Action<VectorVariable, string> OnChangedValueActions = delegate{};
+		#endregion
+
+		#region LifeCycle
+		public VectorVariable(float x, float y) : base(){
+			this ["x"] = new FloatVariable (x);
+			this ["y"] = new FloatVariable (y);
+		}
+		#endregion
+
+		#region Member
+		public FloatVariable X {
+			get{ 
+				return this ["x"];
+			}
+		}
+
+		public FloatVariable Y {
+			get{ 
+				return this ["y"];
+			}
+		}
+
+		public Vector2 AsVector{
+			get{
+				return new Vector2 (this ["x"].AsFloat, this ["y"].AsFloat);
+			}
+
+			set{ 
+				this ["x"].AsFloat = value.x;
+				this ["y"].AsFloat = value.y;
+				NotifyChanged ();
+
+			}
+		}
+		#endregion
+
+		#region Logic
+		public void NotifyChanged(string _message = ""){
+			OnChangedValueActions (this, _message);
+		}
+		#endregion
+	}
+//
+//	public class VirtualVectorVariable : DictionaryContainer<FloatVariable>
+//	{
+//		public FloatVariable X {
+//			get{ 
+//				return this ["x"].AsFloat;
+//			}
+//			set{ 
+//				this ["x"].AsFloat = value;
+//			}
+//		}
+//
+//		public FloatVariable Y {
+//			get{ 
+//				return this ["y"].AsFloat;
+//			}
+//
+//			set{ 
+//				this ["y"].AsFloat = value;
+//			}
+//		}
+//
+//		public Vector2 AsVector{
+//			get{
+//				return new Vector2 (this ["x"].AsFloat, this ["y"].AsFloat);
+//			}
+//
+//			set{ 
+//				this ["x"].AsFloat = value.x;
+//				this ["y"].AsFloat = value.y;
+//			}
+//		}
+//
+//		public VirtualVectorVariable(Action<float> _setter, Func<float> _getter) : base(){
+//			//this ["x"] = new VirtualFloatVariable(
+//			//this ["y"] = new FloatVariable (y);
+//		}
+//	}
+}
