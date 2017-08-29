b0bba6f2769a9134c765161cb7a6a5847d45c14a
LitQoo <litqoo@gmail.com>
2017-08-29 14:56:27 +0900

    [기능] CameraScaler 추가

---
+++ CameraScaler/CameraScaler.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BicUtil.CameraScaler
+{
+	[ExecuteInEditMode]
+	public class CameraScaler : MonoBehaviour {
+		#region LinkingObject
+		[SerializeField]
+		private Camera mainCamera;
+		[SerializeField]
+		private RectTransform referenceTransform;
+		[SerializeField]
+		private Vector2 referenceResolution;
+		[SerializeField]
+		private VerticalAlign verticalAlign = VerticalAlign.Center;
+		[SerializeField]
+		private HorizonalAlign horizonalAlign = HorizonalAlign.Center;
+		#endregion
+
+		#region LifeCycle
+		private void Start(){
+			init ();
+		}
+		#endregion
+
+		#region Logic
+		private void init(){
+			if (referenceTransform != null) {
+				referenceResolution = referenceTransform.sizeDelta;
+			}
+
+			float _referenceRate = referenceResolution.x / referenceResolution.y;
+			float _screenRate = (float)Screen.width / (float)Screen.height;
+
+			if (_referenceRate < _screenRate) {
+				mainCamera.orthographicSize = referenceResolution.y / 2f;
+
+				float _xOffset = 0;
+				switch (horizonalAlign) {
+				case HorizonalAlign.Right:
+					_xOffset = ((float)Screen.width - referenceResolution.x * (float)Screen.height / referenceResolution.y) / 2f;
+					break; 
+				case HorizonalAlign.Left:
+					_xOffset = ((float)Screen.width - referenceResolution.x * (float)Screen.height / referenceResolution.y) / 2f * -1;
+					break; 
+				}
+
+				mainCamera.transform.position = new Vector3 (_xOffset, 0, -10);
+			} else {
+				float _rate = referenceResolution.x / (float)Screen.width;
+				float _hSize = (float)Screen.height * _rate;
+				mainCamera.orthographicSize = _hSize / 2f;
+
+
+				float _yOffset = 0;
+				switch (verticalAlign) {
+				case VerticalAlign.Top:
+					_yOffset = (_hSize - referenceResolution.y) / 2f * -1;
+					break; 
+				case VerticalAlign.Bottom:
+					_yOffset = (_hSize - referenceResolution.y) / 2f;
+					break; 
+				}
+
+				mainCamera.transform.position = new Vector3 (0, _yOffset, -10);
+			}
+
+
+		}
+		#endregion
+
+		#if UNITY_EDITOR
+		private void Update(){
+			init ();
+		}
+		#endif
+	}
+
+	public enum VerticalAlign
+	{
+		Top,
+		Center,
+		Bottom
+	}
+
+	public enum HorizonalAlign
+	{
+		Left,
+		Center,
+		Right
+	}
+}
