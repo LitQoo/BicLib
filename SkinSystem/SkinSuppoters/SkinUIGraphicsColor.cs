b5e50747cb846ecae536d1fc73b18f3f72e6cca3
LitQoo <litqoo@gmail.com>
2017-09-19 23:55:54 +0900

    [기능] SkinSystem.SkinUIGraphicsColor 추가

---
+++ SkinSystem/SkinSuppoters/SkinUIGraphicsColor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace BicUtil.SkinSystem
+{
+	public class SkinUIGraphicsColor : SkinBase {
+		[SerializeField]
+		private SkinUIGraphicInfo[] skinedObjects;
+
+		protected override void applySkin(SkinConfig _config){
+			for (int i = 0; i < skinedObjects.Length; i++) {
+				skinedObjects[i].Graphic.color = _config.SkinData.GetColor (skinedObjects[i].PartName);
+			}
+		}
+	}
+
+	[System.Serializable]
+	public class SkinUIGraphicInfo{
+		public UnityEngine.UI.Graphic Graphic;
+		public string PartName;
+	}
+}
