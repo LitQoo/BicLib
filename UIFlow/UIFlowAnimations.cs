275fa38ddde2e95faab290456d5398ddf1eff090
LitQoo <litqoo@gmail.com>
2017-10-02 12:05:10 +0900

    [기능] UIFlowAnimations 추가 * 자주쓰는 팝업전환 애니메이션 모음 클래스

---
+++ UIFlow/UIFlowAnimations.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace BicUtil.UIFlow
+{
+	static public class UIFlowAnimations {
+		static public void OpenSwipeHorizontal(GameObject _layer, float _firstPosition = -600, float _animationTime = 0.3f){
+			_layer.transform.localPosition = new Vector2 (_firstPosition, 0);
+			LeanTween.moveLocalX (_layer, 0, _animationTime).setEaseInOutQuart ();
+		}
+
+		static public OnCloseUIResult CloseSwipeHorizontal(GameObject _layer, Action _finishCallback, float _firstPosition = -600, float _animationTime = 0.3f){
+			_layer.transform.localPosition = new Vector2 (0, 0);
+			LeanTween.moveLocalX (_layer, _firstPosition, _animationTime).setEaseInOutQuart ().setOnComplete (_finishCallback);
+
+			return OnCloseUIResult.WaitForFinishCallbackAndFastDisplayNext;
+		}
+	}
+}
