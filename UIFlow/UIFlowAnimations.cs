4dcabfdf05f8955dd5730e6aca7b0eedba0e8ff4
LitQoo <litqoo@gmail.com>
2017-10-20 12:59:49 +0900

    [기능] UIFlowAnimations.CloseSwipeHorizontal 파라메터 수정

--- UIFlow/UIFlowAnimations.cs
+++ UIFlow/UIFlowAnimations.cs
@@ -11,7 +11,7 @@ namespace BicUtil.UIFlow
 			LeanTween.moveLocalX (_layer, 0, _animationTime).setEaseInOutQuart ();
 		}
 
-		static public OnCloseUIResult CloseSwipeHorizontal(GameObject _layer, Action _finishCallback, float _firstPosition = -600, float _animationTime = 0.3f){
+		static public OnCloseUIResult CloseSwipeHorizontal(GameObject _layer, Action _finishCallback = null, float _firstPosition = -600, float _animationTime = 0.3f){
 			_layer.transform.localPosition = new Vector2 (0, 0);
 			LeanTween.moveLocalX (_layer, _firstPosition, _animationTime).setEaseInOutQuart ().setOnComplete (_finishCallback);
 
