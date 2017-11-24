1b98876defa6a8a0f645e88ac006be1f7959568b
LitQoo <litqoo@gmail.com>
2017-11-24 11:23:04 +0900

    [디버깅] ClassInitializer newline오류 수정

--- ClassInitializer/ClassInitializer.cs
+++ ClassInitializer/ClassInitializer.cs
@@ -1,1 +1,46 @@
-﻿using System.Collections; using System.Collections.Generic; using UnityEngine; using System.Linq;  namespace BicUtil.ClassInitializer { 	public class ClassInitializer : MonoBehaviour { 		#region LinkingObject 		[SerializeField] 		private List<ClassInitializerObjectInfo> initObjects; 		#endregion  		#region LifeCycle 		private void Awake(){ 			initialize (); 		} 		#endregion  		#region logic 		private void initialize(){  			var _orderedList = initObjects.OrderBy (_object => _object.Order);  			foreach (var _item in _orderedList) { 				if (_item.Target != null) { 					IClassInitializerObject[] _initObjects = _item.Target.GetComponents<IClassInitializerObject> (); 					for (int i = 0; i < _initObjects.Length; i++){ 						_initObjects[i].Initialize (); 					} 				} 			} 		} 		#endregion 	}  	public interface IClassInitializerObject 	{ 		void Initialize (); 	}  	[System.Serializable] 	public class ClassInitializerObjectInfo{ 		public GameObject Target; 		public int Order; 	} }
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace BicUtil.ClassInitializer
+{
+	public class ClassInitializer : MonoBehaviour {
+		#region LinkingObject
+		[SerializeField]
+		private List<ClassInitializerObjectInfo> initObjects;
+		#endregion
+
+		#region LifeCycle
+		private void Awake(){
+			initialize ();
+		}
+		#endregion
+
+		#region logic
+		private void initialize(){
+			var _orderedList = initObjects.OrderBy (_object => _object.Order);
+
+			foreach (var _item in _orderedList) {
+				if (_item.Target != null) {
+					IClassInitializerObject[] _initObjects = _item.Target.GetComponents<IClassInitializerObject> ();
+					for (int i = 0; i < _initObjects.Length; i++){
+						_initObjects[i].Initialize ();
+					}
+				}
+			}
+		}
+		#endregion
+	}
+
+	public interface IClassInitializerObject
+	{
+		void Initialize ();
+	}
+
+	[System.Serializable]
+	public class ClassInitializerObjectInfo{
+		public GameObject Target;
+		public int Order;
+	}
+}
