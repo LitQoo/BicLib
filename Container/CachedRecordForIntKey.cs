1a744fdaa2e5ffc2782da087cc39302f2102bc7f
LitQoo <litqoo@gmail.com>
2018-09-14 04:37:07 +0900

    [리펙토링] CachedRecordForIntKey 멤버형 변경 (IntVariable=>IVariable)

--- Container/CachedRecordForIntKey.cs
+++ Container/CachedRecordForIntKey.cs
@@ -5,36 +5,36 @@ using System;
 namespace BicDB.Container
 {
     public class CachedRecordForIntKey<T> where T : class, IRecordContainer, new(){
-        private IntVariable foreignKeyField;
-        private IntVariable targetField;
+        private IVariable foreignKeyField;
+        private IVariable targetField;
         private string targetFieldName;
         private ITableContainer<T> targetTable;
-        private T value = null;
+        private T record = null;
         
-        public T Value{
+        public T Record{
             get{
-                if(value == null){
+                if(record == null){
                     cached();
                 }else if(targetField != null && foreignKeyField.AsInt != targetField.AsInt){
                     cached();
                 }
 
-                return value;
+                return record;
             }
         }
 
         private void cached(){
-            value = targetTable.FirstOrDefault(_row=>_row[targetFieldName].AsVariable.AsInt == this.foreignKeyField.AsInt);
+            record = targetTable.FirstOrDefault(_row=>_row[targetFieldName].AsVariable.AsInt == this.foreignKeyField.AsInt);
             
-            if(value != null){
-                targetField = value[targetFieldName].As<IntVariable>();
+            if(record != null){
+                targetField = record[targetFieldName].AsVariable;
             }else{
                 targetField = null;
             }
         }
 
         //_foreignKeyField 와 _targetTable의 레코드 멤버 _targetFieldName 를 비교하여 Value를 최신으로 캐쉬
-        public CachedRecordForIntKey(IntVariable _foreignKeyField, ITableContainer<T> _targetTable, string _targetFieldName){
+        public CachedRecordForIntKey(IVariable _foreignKeyField, ITableContainer<T> _targetTable, string _targetFieldName){
             foreignKeyField = _foreignKeyField;
             targetTable = _targetTable;
             targetFieldName = _targetFieldName;
