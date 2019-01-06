d95d418155fe4e190aedccca9c5c104956a430a3
LitQoo <litqoo@gmail.com>
2019-01-07 02:38:22 +0900

    [Feature] TouchNotifier 모바일 멀티 터치 지원

--- TouchNotifier/TouchNotifier.cs
+++ TouchNotifier/TouchNotifier.cs
@@ -11,61 +11,57 @@ namespace BicUtil.TouchNotifier
 		#region Event
 		public const string BUTTON_ESC = "esc";
 
-		[System.Serializable]
-		public class TouchEvent : UnityEvent<Vector2> {}
-		[System.Serializable]
-		public class TouchEventWithStartingPosition : UnityEvent<Vector2, Vector2>{}
-
-		public TouchEvent OnTouchDown;
-		public TouchEvent OnTouchMove;
-		public TouchEvent OnTouchUp;
-		public TouchEvent OnTouchBackKey;
 		
-		public TouchEventWithStartingPosition OnTouchMoveWithStartingPosition;
-		public TouchEventWithStartingPosition OnTouchUpWithStartingPosition;
+		[System.Serializable]
+		public class BackKeyEvent : UnityEvent<Vector2> {}
+		[System.Serializable]
+		public class TouchEvent : UnityEvent<Vector2, int> {}
+		[System.Serializable]
+		public class TouchEventWithStartingPosition : UnityEvent<Vector2, Vector2, int>{}
+
+		public BackKeyEvent OnTouchBackKey;
+		public TouchEvent OnTouchDown;
+		public TouchEventWithStartingPosition OnTouchMove;
+		public TouchEventWithStartingPosition OnTouchUp;
 		private Vector2[] startTouchPosition = new Vector2[5];
 
-		private bool isTouchIn = false;
+		private bool[] isTouchIn = new bool[5]{false, false, false, false, false};
 
 		private void Update () {
 			#if UNITY_EDITOR
 			if (Input.GetMouseButtonDown (0)) {
 				if (UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject () == false) {
 					startTouchPosition[0] = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-					OnTouchDown.Invoke (startTouchPosition[0]);
-					
-					isTouchIn = true;
-				}
-			} else if (Input.GetMouseButtonUp (0) && isTouchIn == true) {
-				Vector2 _position = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-				OnTouchUp.Invoke (_position);
-				OnTouchUpWithStartingPosition.Invoke (startTouchPosition[0], _position);
+					OnTouchDown.Invoke (startTouchPosition[0], 0);
 
-				isTouchIn = false;
-			} else if (isTouchIn == true) {
+					isTouchIn[0] = true;
+				}
+			} else if (Input.GetMouseButtonUp (0) && isTouchIn[0] == true) {
 				Vector2 _position = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-				OnTouchMove.Invoke (_position);
-				OnTouchMoveWithStartingPosition.Invoke(startTouchPosition[0], _position);
+				OnTouchUp.Invoke (startTouchPosition[0], _position, 0);
+				isTouchIn[0] = false;
+			} else if (isTouchIn[0] == true) {
+				Vector2 _position = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+				OnTouchMove.Invoke (startTouchPosition[0],_position, 0);
 			}
 
 			#else
 			if(Input.touchCount > 0){
 				for(int i = 0; i < Input.touchCount; i++){
-					if(Input.GetTouch(i).phase == TouchPhase.Began){
+					var _touch = Input.GetTouch(i);
+					if(_touch.phase == TouchPhase.Began){
 						if(UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject (i) == false){
-							startTouchPosition[i] = Camera.main.ScreenToWorldPoint (Input.GetTouch(i).position);
-							OnTouchDown.Invoke (startTouchPosition[i]);
-							isTouchIn = true;
+							startTouchPosition[i] = Camera.main.ScreenToWorldPoint (_touch.position);
+							OnTouchDown.Invoke (startTouchPosition[i], i);
+							isTouchIn[i] = true;
 						}
-					} else if(Input.GetTouch(i).phase == TouchPhase.Ended && isTouchIn == true){
-						Vector2 _position = Camera.main.ScreenToWorldPoint (Input.GetTouch(i).position);
-						OnTouchUp.Invoke (_position);
-						OnTouchUpWithStartingPosition.Invoke(startTouchPosition[i], _position);
-						isTouchIn = false;
-					} else if(isTouchIn == true){
-						Vector2 _position = Camera.main.ScreenToWorldPoint (Input.GetTouch(i).position);
-						OnTouchMove.Invoke (_position);
-						OnTouchMoveWithStartingPosition.Invoke(startTouchPosition[i], _position);
+					} else if(_touch.phase == TouchPhase.Ended && isTouchIn[i] == true){
+						Vector2 _position = Camera.main.ScreenToWorldPoint (_touch.position);
+						OnTouchUp.Invoke(startTouchPosition[i], _position, i);
+						isTouchIn[i] = false;
+					} else if(isTouchIn[i] == true){
+						Vector2 _position = Camera.main.ScreenToWorldPoint (_touch.position);
+						OnTouchMove.Invoke(startTouchPosition[i], _position, i);
 					}
 				}
 			}
