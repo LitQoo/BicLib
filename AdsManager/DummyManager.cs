4743e32829f3ca66a5d6b76c841fd4e3b71323d0
LitQoo <litqoo@gmail.com>
2019-01-26 02:33:17 +0900

    [Feature] AdsManager webgl용 DummyManager 추가

---
+++ AdsManager/DummyManager.cs
@@ -0,0 +1,74 @@
+
+using System;
+
+namespace BicUtil.AdsManager{
+
+    public class DummyManager : IAdsManager
+    {
+        private IAdsManager instance = null;
+        public IAdsManager Instance{
+            get{
+                if(instance == null){
+                    instance = new DummyManager();
+                }
+
+                return instance;
+            }
+        } 
+        
+        public void Initialize()
+        {
+            
+        }
+
+        public bool IsReadyInterstitial(object _adsType)
+        {
+            return false;
+        }
+
+        public bool IsReadyRewardBased(object _adsType)
+        {
+            return false;
+        }
+
+        public void LoadInterstitial(object _adsType)
+        {
+            
+        }
+
+        public void LoadRewardBased(object _adsType)
+        {
+            
+        }
+
+        public void SetAdsSettingAndroidOnly(string _adsId, object _type, int _playTimeInterval)
+        {
+            
+        }
+
+        public void SetAdsSettingIOSOnly(string _adsId, object _type, int _playTimeInterval)
+        {
+            
+        }
+
+        public void SetPlatformAndroid(string _androidId)
+        {
+            
+        }
+
+        public void SetPlatformIos(string _iosId)
+        {
+            
+        }
+
+        public void ShowInterstitial(object _adsType, Action<AdsResult> _callback)
+        {
+            _callback(AdsResult.Finished);
+        }
+
+        public void ShowRewardBased(object _adsType, Action<AdsResult> _callback)
+        {
+            _callback(AdsResult.Finished);
+        }
+    }
+}
