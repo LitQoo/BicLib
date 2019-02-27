9d539f184ef1e8905f63ee8209b50bc7c8ac1adb
LitQoo <litqoo@gmail.com>
2019-02-27 13:02:22 +0900

    [Feature] PurchainsgManager 프로덕트 구입 콜백 추가

--- IAP/DummyManager.cs
+++ IAP/DummyManager.cs
@@ -3,6 +3,7 @@ using System;
 using System.Linq;
 using BicDB.Container;
 using BicDB.Storage;
+using BicDB.Variable;
 using UnityEngine.Purchasing;
 
 namespace BicUtil.Purchasing{
@@ -23,7 +24,7 @@ namespace BicUtil.Purchasing{
         public TableContainer<ProductModel<PRODUCTTYPE>> ProductTable{get{return productTable;}}
         public bool isLoadedProductTable = false;
 
-        public void AddProduct(PRODUCTTYPE _idType, string _id, ProductType _productType, int _value)
+        public void AddProduct(PRODUCTTYPE _idType, string _id, ProductType _productType, int _value, Action<IVariable> _valueChangedCallback)
         {
             if(isLoadedProductTable == false){
 				ProductTable.SetStorage(FileStorage.GetInstance());
@@ -39,6 +40,11 @@ namespace BicUtil.Purchasing{
             }
 
             _product.ProductType.AsEnum = _productType;
+
+            if(_valueChangedCallback != null){
+                _product.Value.OnChangedValueActions += _valueChangedCallback;
+                _product.Value.NotifyChanged();
+            }
         }
 
         public void BuyProduct(PRODUCTTYPE _idType, Action<PurchasingResult> _callback)
