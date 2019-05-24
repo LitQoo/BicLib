0554a0948b2c76bf3b8007862bb4d6f48c6d220c
LitQoo <litqoo@gmail.com>
2019-05-24 17:33:12 +0900

    [Feature] CameraScaler Offset 추가 * 배너 공간을 쉽게 확보/제거 할 수 있도록 함

--- CameraScaler/CameraScaler.cs
+++ CameraScaler/CameraScaler.cs
@@ -6,6 +6,9 @@ namespace BicUtil.CameraScaler
 {
 	[ExecuteInEditMode]
 	public class CameraScaler : MonoBehaviour {
+		#region static
+		static public CameraScaler Instance;
+		#endregion
 		#region LinkingObject
 		[SerializeField]
 		private Camera mainCamera;
@@ -23,6 +26,7 @@ namespace BicUtil.CameraScaler
 
 		#region LifeCycle
 		private void Start(){
+			Instance = this;
 			init ();
 		}
 		#endregion
@@ -38,7 +42,8 @@ namespace BicUtil.CameraScaler
 				referenceResolution = referenceTransform.sizeDelta;
 			}
 
-			float _referenceRate = referenceResolution.x / referenceResolution.y;
+			Vector2 calcresolution = referenceResolution + manageFullSizeOffset;
+			float _referenceRate = calcresolution.x / calcresolution.y;
 			float _screenRate = (float)Screen.width / (float)Screen.height;
 
 			//설정보다 뚱뚱할때
@@ -55,16 +60,27 @@ namespace BicUtil.CameraScaler
 					break; 
 				}
 
-				mainCamera.transform.position = new Vector3 (_xOffset, 0, -10);
+				float _yOffset = 0;
+				if(manageFullSizeOffset.y != 0){
+					switch (verticalAlign) {
+						case VerticalAlign.Top:
+						_yOffset = manageFullSizeOffset.y / 2f;
+						break;
+						case VerticalAlign.Bottom:
+						_yOffset = - manageFullSizeOffset.y / 2f;
+						break;
+					}
+				}
 
+				mainCamera.transform.position = new Vector3 (_xOffset, _yOffset, -10);
 
 				if(manageFullSizeRect != null){
 					float _rate = referenceResolution.y / (float)Screen.height;
-					manageFullSizeRect.sizeDelta = new Vector3((float)Screen.safeArea.width * _rate, (float)Screen.safeArea.height * _rate);
+					manageFullSizeRect.sizeDelta = new Vector2((float)Screen.safeArea.width * _rate, (float)Screen.safeArea.height * _rate) + manageFullSizeOffset;
 				}
 
 			//설정보다 길쭉할때
-			} else if(_referenceRate > _screenRate){
+			} else {
 				float _rate = referenceResolution.x / (float)Screen.width;
 				float _hSize = (float)Screen.height * _rate;
 				mainCamera.orthographicSize = _hSize / 2f;
@@ -73,29 +89,59 @@ namespace BicUtil.CameraScaler
 				float _yOffset = 0;
 				switch (verticalAlign) {
 				case VerticalAlign.Top:
-					_yOffset = (_hSize - referenceResolution.y) / 2f * -1;
+					if(manageFullSizeOffset.y != 0){
+						_yOffset = manageFullSizeOffset.y / 2f;
+					}else{
+						_yOffset = (_hSize - referenceResolution.y) / 2f * -1;
+					}
 					break; 
 				case VerticalAlign.Bottom:
-					_yOffset = (_hSize - referenceResolution.y) / 2f;
+					if(manageFullSizeOffset.y != 0){
+						_yOffset = - manageFullSizeOffset.y / 2f;
+					}else{
+						_yOffset = (_hSize - referenceResolution.y) / 2f;
+					}
 					break; 
 				}
 
 				mainCamera.transform.position = new Vector3 (0, _yOffset, -10);
 
 				if(manageFullSizeRect != null){
-					manageFullSizeRect.sizeDelta = new Vector3((float)Screen.safeArea.width * _rate, (float)Screen.safeArea.height * _rate);
+					var _rectRate = referenceResolution.x / (float)Screen.width;
+					manageFullSizeRect.sizeDelta = new Vector2((float)Screen.safeArea.width * _rectRate, (float)Screen.safeArea.height * _rectRate) + manageFullSizeOffset;
 				}
 			}
+
+			#if UNITY_EDITOR
+			string _log = "[CameraScaler] Screen size change detected. " + Screen.safeArea.size.ToString();
+			_log += "\nmainCamera position to " + mainCamera.transform.position.ToString();
+			if(manageFullSizeRect != null){
+				_log += "\nmanageFullSizeRect to " + manageFullSizeRect.sizeDelta.ToString();
+				_log += "\noffset to " + manageFullSizeOffset.ToString();
+			}
+
+			Debug.LogWarning(_log);
+			#endif
+		}
+		
+		private Vector2 manageFullSizeOffset = Vector2.zero;
+
+		public void SetManageFullSizeOffset(Vector2 _offset){
+			manageFullSizeOffset = _offset;
+			init();
 		}
 		#endregion
 
 		#if UNITY_EDITOR
+		Rect screenSize = Rect.zero;
+
+		[ExecuteInEditMode]
 		private void Update(){
-			if(manageFullSizeRect != null){
+			if(screenSize.Equals(Screen.safeArea) == false){
+				screenSize = Screen.safeArea;
+				init();
 				return;
 			}
-			
-			init ();
 		}
 		#endif
 	}
