9560f5e85a487d35979d916f7af14f79abd42b57
LitQoo <litqoo@gmail.com>
2019-07-25 19:45:36 +0900

    [Fix] TableLoader 에러메세지 보충

--- DB/Core/TableLoader.cs
+++ DB/Core/TableLoader.cs
@@ -53,7 +53,7 @@ namespace BicDB.Core
                 _loadData.Table.Load(_result=>{
                     if(_result.Code != 0){
                         tableList.Add(_loadData);
-                        errorMasssage = _loadData.Table.Name + "/" + leftCount.ToString() + "/" + _result.Message;
+                        errorMasssage += _loadData.Table.Name + "/" + leftCount.ToString() + "/" + _result.Message + "/" + _result.Code.ToString() +"\n";
                     }else{
                         if(_loadData.SuccessCallback != null){
                             _loadData.SuccessCallback();
