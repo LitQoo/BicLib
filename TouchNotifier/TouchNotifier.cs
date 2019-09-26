cf4f516e9a8869b56469b8b557c0900766ca7bc2
LitQoo <litqoo@gmail.com>
2019-09-26 22:10:36 +0900

    [Feature] TouchNotifier.IsIn 추가

--- TouchNotifier/TouchNotifier.cs
+++ TouchNotifier/TouchNotifier.cs
@@ -28,62 +28,62 @@ namespace BicUtil.TouchNotifier
 		private bool[] isTouchIn = new bool[10]{false, false, false, false, false,false, false, false, false, false};
 
 		private void Update () {
-			try{
-				#if UNITY_EDITOR || UNITY_WEBGL || UNITY_STANDALONE || UNITY_FACEBOOK
-				if (Input.GetMouseButtonDown (0)) {
-					if (UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject () == false) {
-						startTouchPosition[0] = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-						OnTouchDown.Invoke (startTouchPosition[0], 0);
+			#if UNITY_EDITOR || UNITY_WEBGL || UNITY_STANDALONE || UNITY_FACEBOOK
+			if (Input.GetMouseButtonDown (0)) {
+				if (UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject () == false) {
+					startTouchPosition[0] = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+					OnTouchDown.Invoke (startTouchPosition[0], 0);
 
-						isTouchIn[0] = true;
-					}
-				} else if (Input.GetMouseButtonUp (0) && isTouchIn[0] == true) {
-					Vector2 _position = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-					OnTouchUp.Invoke (startTouchPosition[0], _position, 0);
-					isTouchIn[0] = false;
-				} else if (isTouchIn[0] == true) {
-					Vector2 _position = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-					if(startTouchPosition[0] != _position){
-						OnTouchMove.Invoke (startTouchPosition[0], _position, 0);
-					}
+					isTouchIn[0] = true;
 				}
+			} else if (Input.GetMouseButtonUp (0) && isTouchIn[0] == true) {
+				Vector2 _position = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+				OnTouchUp.Invoke (startTouchPosition[0], _position, 0);
+				isTouchIn[0] = false;
+			} else if (isTouchIn[0] == true) {
+				Vector2 _position = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+				if(startTouchPosition[0] != _position){
+					OnTouchMove.Invoke (startTouchPosition[0], _position, 0);
+				}
+			}
 
-				#else
-				if(Input.touchCount > 0){
-					for(int i = 0; i < Input.touchCount; i++){
-						var _touch = Input.GetTouch(i);
-						if(_touch.phase == TouchPhase.Began){
-							if(UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject (i) == false){
-								startTouchPosition[i] = Camera.main.ScreenToWorldPoint (_touch.position);
-								OnTouchDown.Invoke (startTouchPosition[i], i);
-								isTouchIn[i] = true;
-							}
-						} else if(_touch.phase == TouchPhase.Ended && isTouchIn[i] == true){
-							Vector2 _position = Camera.main.ScreenToWorldPoint (_touch.position);
-							OnTouchUp.Invoke(startTouchPosition[i], _position, i);
-							isTouchIn[i] = false;
-						} else if(isTouchIn[i] == true){
-							Vector2 _position = Camera.main.ScreenToWorldPoint (_touch.position);
-							if(_position != startTouchPosition[i]){
-								OnTouchMove.Invoke(startTouchPosition[i], _position, i);
-							}
+			#else
+			if(Input.touchCount > 0){
+				for(int i = 0; i < Input.touchCount; i++){
+					var _touch = Input.GetTouch(i);
+					if(_touch.phase == TouchPhase.Began){
+						if(UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject (i) == false){
+							startTouchPosition[i] = Camera.main.ScreenToWorldPoint (_touch.position);
+							OnTouchDown.Invoke (startTouchPosition[i], i);
+							isTouchIn[i] = true;
+						}
+					} else if(_touch.phase == TouchPhase.Ended && isTouchIn[i] == true){
+						Vector2 _position = Camera.main.ScreenToWorldPoint (_touch.position);
+						OnTouchUp.Invoke(startTouchPosition[i], _position, i);
+						isTouchIn[i] = false;
+					} else if(isTouchIn[i] == true){
+						Vector2 _position = Camera.main.ScreenToWorldPoint (_touch.position);
+						if(_position != startTouchPosition[i]){
+							OnTouchMove.Invoke(startTouchPosition[i], _position, i);
 						}
 					}
 				}
-				#endif
+			}
+			#endif
 
-				if(Input.GetKeyUp(KeyCode.Escape))
-				{
-					OnTouchBackKey.Invoke(Vector2.zero);
-					EventNotifyer.EventNotifyer.Notify(this, BUTTON_ESC);
-				}
-			}catch(SystemException _e){
-				Debug.LogWarning("[TouchNotifier] Exception " + _e.Message + "/ touchCount = " + Input.touchCount.ToString());
+			if(Input.GetKeyUp(KeyCode.Escape))
+			{
+				OnTouchBackKey.Invoke(Vector2.zero);
+				EventNotifyer.EventNotifyer.Notify(this, BUTTON_ESC);
 			}
 		}
 
 		private void OnDestroy() {
 			EventNotifyer.EventNotifyer.ClearSubscription(this);
+		}
+
+		public bool IsIn(RectTransform _rectTransform, Vector2 _worldPosition){
+			return UnityEngine.RectTransformUtility.RectangleContainsScreenPoint(_rectTransform, _worldPosition);
 		}
 		#endregion
 	}
