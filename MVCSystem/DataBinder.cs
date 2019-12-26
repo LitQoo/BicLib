00f2fac6fd1f6cd9856e1b847ab6e15d8a238da6
LitQoo <litqoo@gmail.com>
2019-12-26 18:02:19 +0900

    [Feature] DataBinder 추가 * ModelBinder 좀더 편하게 쓰도록

---
+++ MVCSystem/DataBinder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BicDB.Variable;
+using System;
+using BicDB;
+using BicDB.Container;
+using BicUtil.ListValueSelector;
+
+namespace BicUtil.MVCSystem
+{
+	public class DataBinder<T> : ModelBinder where T : class
+	{
+        public T Data{
+            get=>data;
+        }
+
+        private T data;
+
+        public void Bind(T _data){
+            if(this.data != null){
+                this.ClearBinding();
+            }
+            
+            this.data = _data;
+        }
+
+        public new void ClearBinding(){
+            base.ClearBinding();
+            this.data = null;
+        }
+    }
+}
