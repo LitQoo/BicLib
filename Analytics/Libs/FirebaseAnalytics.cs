08a317550008e19deab3578e75130e44ece91eb1
LitQoo <litqoo@gmail.com>
2020-02-10 00:03:19 +0900

    [Fix] FirebaseAnalytics 파라메터 없을경우 처리

--- Analytics/Libs/FirebaseAnalytics.cs
+++ Analytics/Libs/FirebaseAnalytics.cs
@@ -22,21 +22,38 @@ namespace BicUtil.Analytics
         private bool needRetryEvent = false;
         private List<SavedEvent> savedEvent = new List<SavedEvent>();
         public void Event(string _name, Dictionary<string, object> _eventData = null, int _value = 1){
+            UnityEngine.Debug.Log("firebase event 1");
             var _isAvailable = Firebase.DependencyStatus.UnavilableMissing;
-
+            UnityEngine.Debug.Log("firebase event 2");
             try{
+                UnityEngine.Debug.Log("firebase event 3");
                 _isAvailable = Firebase.FirebaseApp.CheckDependencies();
+                UnityEngine.Debug.Log("firebase event 4");
             }catch{
+                UnityEngine.Debug.Log("firebase event 5");
                 _isAvailable = Firebase.DependencyStatus.UnavilableMissing;
+                UnityEngine.Debug.Log("firebase event 6");
             }
             
+            UnityEngine.Debug.Log("firebase event 7");
             if(_isAvailable == Firebase.DependencyStatus.Available){
+            UnityEngine.Debug.Log("firebase event 8");
                 RetrySavedEvent();
+
+                UnityEngine.Debug.Log("firebase event 9");
                 sendEvent(_name, _value, _eventData);
+
+                UnityEngine.Debug.Log("firebase event 10");
             }else{
+
+                UnityEngine.Debug.Log("firebase event 11");
                 savedEvent.Add(new SavedEvent(_name, _eventData, _value));
                 needRetryEvent = true;
+
+                UnityEngine.Debug.Log("firebase event 12");
             }
+
+            UnityEngine.Debug.Log("firebase event 13");
         }
 
         public void RetrySavedEvent(){
@@ -51,14 +68,19 @@ namespace BicUtil.Analytics
         }
 
         private void sendEvent(string _eventName, int _value, Dictionary<string, object> _data){
-            
-            List<Firebase.Analytics.Parameter> _params = new List<Firebase.Analytics.Parameter>();
+            if(_data != null){
+                List<Firebase.Analytics.Parameter> _params = new List<Firebase.Analytics.Parameter>();
 
-            foreach(var _param in _data){
-                _params.Add(new Firebase.Analytics.Parameter(_param.Key, _param.Value.ToString()));
+                foreach(var _param in _data){
+
+                    UnityEngine.Debug.Log("firebase sendEvent " + _param.Key);
+                    _params.Add(new Firebase.Analytics.Parameter(_param.Key, _param.Value.ToString()));
+                }
+
+                Firebase.Analytics.FirebaseAnalytics.LogEvent(_eventName, _params.ToArray());
+            }else{
+                Firebase.Analytics.FirebaseAnalytics.LogEvent(_eventName);
             }
-           
-            Firebase.Analytics.FirebaseAnalytics.LogEvent(_eventName, _params.ToArray());
         }
     }
 }
