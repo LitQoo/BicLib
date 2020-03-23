83dc7f3d4a162c2d6b0a6318e59753b4c1a8152f
LitQoo <litqoo@gmail.com>
2020-03-24 03:16:58 +0900

    [Feature] UI ActiveSelect, NumberChange 추가

---
+++ UI/Scripts/ActiveSelect.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using BicDB.Variable;
+using UnityEngine;
+
+namespace BicUtil.UI{
+    public class ActiveSelect : MonoBehaviour
+    {
+        #region DI
+        [SerializeField]
+        private GameObject[] enableObjects;
+        #endregion
+
+        public IntVariable SelectedIndex{get;set;} = new IntVariable(0);
+
+        private void Awake(){
+            this.SelectedIndex.Subscribe(enableObject, true);
+        }
+
+        private void enableObject(IVariable _selectedIndexk)
+        {
+            for(int i = 0; i < enableObjects.Length; i++){
+                enableObjects[i].SetActive(i == _selectedIndexk.AsInt);
+            }
+        }
+
+        public void IncreaseIndex(){
+            var _nextValue = SelectedIndex.AsInt + 1;
+            if(_nextValue >= enableObjects.Length){
+                _nextValue = 0;
+            }
+            
+            SelectedIndex.AsInt = Mathf.Clamp(_nextValue, 0, enableObjects.Length - 1);
+        
+        }
+    }
+}
---
+++ UI/Scripts/NumberChange.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using BicDB.Variable;
+using UnityEngine;
+
+namespace BicUtil.UI{
+    public class NumberChange : MonoBehaviour
+    {
+        #region Setting
+        [SerializeField]
+        private int minValue;
+        [SerializeField]
+        private int maxValue;
+        [SerializeField]
+        private int gapValue;
+        [SerializeField]
+        private string valueFormat = "{0}";
+        [SerializeField]
+        private UnityEngine.UI.Text valueText;
+        #endregion
+
+        public IntVariable Value{get;set;} = new IntVariable(0);
+
+        private void Awake(){
+            this.Value.Subscribe(setValueText, true);
+        }
+
+        public void Add(){
+            this.Value.AsInt = Mathf.Clamp(this.Value.AsInt + gapValue, minValue, maxValue);
+        }
+
+        public void Sub(){
+            this.Value.AsInt = Mathf.Clamp(this.Value.AsInt - gapValue, minValue, maxValue);
+        }
+
+        private void setValueText(IVariable _value)
+        {
+            this.valueText.text = string.Format(valueFormat, this.Value.AsInt);
+        }
+    }
+}
