0e514d5c83d0bbbafa6503689050095bed8815f8
LitQoo <litqoo@gmail.com>
2020-07-16 18:16:40 +0900

    [Refactor] ForeignRecord 캐시기능 추가

--- DB/Container/ForeignRecord.cs
+++ DB/Container/ForeignRecord.cs
@@ -3,30 +3,42 @@ using System.Collections.Generic;
 using BicDB.Container;
 using BicDB.Variable;
 using System.Linq;
+using System;
 
 namespace BicDB.Container
 {
-	public class ForeignRecord<T> where T : IRecordContainer{
+	public class ForeignRecord<T> where T : class, IRecordContainer{
 		private ITableContainer<T> targetTable;
 		private string targetFieldName;
-		private IVariable findVariable;
-		private T cached = default(T);
-		public ForeignRecord(ITableContainer<T> _targetTable, string _findFieldName, IVariable _findVariable){
+		private IVariableReadOnly idVariable;
+		private string lastIdValue = "_-_n_o-n_e_-_";
+		private T cached = null;
+		private Func<IVariableReadOnly,T> defaultFunc = null;
+
+		public ForeignRecord(ITableContainer<T> _targetTable, string _findFieldName, IVariableReadOnly _idVariable, Func<IVariableReadOnly, T> _defaultFunc = null){
 			targetTable = _targetTable;
 			targetFieldName = _findFieldName;
-			findVariable = _findVariable;
-
-			findVariable.Subscribe(findRecord, true);
+			idVariable = _idVariable;
+			defaultFunc = _defaultFunc;
 		}
 
-		T Record{
+		public T Record{
 			get{
+				if(lastIdValue != idVariable.AsString){
+					findRecord(this.idVariable);
+				}
+
 				return cached;
 			}
 		}
 
-		private void findRecord(IVariableReadOnly _findVariable){
-			cached = targetTable.FirstOrDefault(_row=>_row[targetFieldName].AsVariable.AsString == findVariable.AsString);
+		private void findRecord(IVariableReadOnly _idVariable){
+			lastIdValue = _idVariable.AsString;
+			cached = targetTable.FirstOrDefault(_row=>_row[targetFieldName].AsVariable.AsString == idVariable.AsString);
+
+			if(cached == null && defaultFunc != null){
+				cached = defaultFunc(_idVariable);
+			}
 		}
 	}
 }
