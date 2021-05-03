665025210a7ea1e67fb799a40003bb5e6cc986d3
LitQoo <litqoo@gmail.com>
2021-05-03 21:24:51 +0900

    [Feature] BicDB.Inspector RecordContainer 지원

--- DB/Core/Editor/InspectorEditor.cs
+++ DB/Core/Editor/InspectorEditor.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
+using BicDB.Container;
 using BicDB.Core;
 using BicDB.Variable;
 using UnityEditor;
@@ -21,6 +22,7 @@ namespace BicUtil.Core{
 
 
         string input = "";
+        Vector2 scroll = new Vector2(0, 0);
         private void OnGUI()
         {
             if(GUILayout.Button("Reload") == true){
@@ -32,8 +34,6 @@ namespace BicUtil.Core{
                 
                 return;
             }
-
-            GUILayout.BeginVertical();
             var _inspector = (TableService.Inspector as Inspector);
 
             if(_inspector == null){
@@ -42,51 +42,75 @@ namespace BicUtil.Core{
             }
 
 
-            foreach(var _keyValue in _inspector.TrackingList){
-                var _object = _keyValue.Value;
+            scroll = GUILayout.BeginScrollView(scroll);
+            GUILayout.BeginVertical();
 
-                Type myType = _object.GetType();
-                var _fieldList = myType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-                
-                GUILayout.Label(_keyValue.Key, EditorStyles.boldLabel);
-                
-                for (int i =0 ; i < _fieldList.Length ; i++)
-                {
-                    
-                    GUILayout.BeginHorizontal();
-                    // Display name and type of the concerned member.
-                    var _fieldType = _fieldList[i].FieldType.ToString();
-                    if(_fieldType.Contains(".")){
-                        var _split = _fieldType.Split('.');
-                        _fieldType = _split[_split.Length - 1];
-                    }
-                    GUILayout.Label(_fieldType, EditorStyles.largeLabel, GUILayout.MinWidth(100));
-                    GUILayout.Label(_fieldList[i].Name.ToString(), EditorStyles.largeLabel, GUILayout.MinWidth(100));
-
-                    var _field = _fieldList[i].GetValue(_object);
-
-                    if(_field is IVariable){
-                        var _variable = (_field as IVariable);
-                        GUILayout.Label(_variable.AsString, EditorStyles.largeLabel);
-                        input = GUILayout.TextField(input);
-
-                        if(GUILayout.Button("Set", GUILayout.MaxWidth(30)) == true){
-                            _variable.AsString = input; 
-                            input = "";
-                        }
-                    } 
-                    GUILayout.EndHorizontal();
-                }
+            foreach(var _keyValue in _inspector.TrackingList)
+            {
+                inspectObject(_keyValue.Value, _keyValue.Key);
             }
+
             GUILayout.EndVertical();
+            GUILayout.EndScrollView();
+        }
 
-            // GUILayout.BeginVertical();
-            // GUILayout.Label("Define Symbol", EditorStyles.largeLabel);
-            // GUILayout.BeginHorizontal();
-            // GUILayout.Label("", EditorStyles.boldLabel, GUILayout.Height(15));
+        private void inspectObject(object _object, string _id)
+        {
+            Type myType = _object.GetType();
+            var _fieldList = myType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
 
-            // GUILayout.EndHorizontal();
-            // GUILayout.EndVertical();
+            GUILayout.Label(_id, EditorStyles.boldLabel);
+
+            for (int i = 0; i < _fieldList.Length; i++)
+            {
+                inspectField(_object, _fieldList[i]);
+            }
+        }
+
+        private void inspectField(object _object, FieldInfo _fieldInfo)
+        {
+
+            var _field = _fieldInfo.GetValue(_object);
+            var _name = _fieldInfo.Name.ToString();
+
+            if(_name.Contains("<") == true){
+                return;
+            }
+
+            GUILayout.BeginHorizontal();
+
+
+            GUILayout.Space(20);
+            if (_field is IVariable)
+            {
+                inspectVariable(_field as IVariable, _name);
+            }else if(_field is IRecordContainer){
+                inspectRecord(_field as IRecordContainer, _name);
+            }
+
+            GUILayout.EndHorizontal();
+        }
+
+        private void inspectRecord(IRecordContainer _record, string _name){
+            
+            GUILayout.BeginVertical();
+            inspectObject(_record, _name);
+            GUILayout.EndVertical();
+        }
+
+        private void inspectVariable(IVariable _variable, string _name)
+        {
+            GUILayout.Label(_variable.Type.ToString(), EditorStyles.largeLabel, GUILayout.MinWidth(50));
+            GUILayout.Label(_name, EditorStyles.largeLabel, GUILayout.MinWidth(100));
+            GUILayout.Label(_variable.AsString, EditorStyles.largeLabel);
+
+            input = GUILayout.TextField(input);
+
+            if (GUILayout.Button("Set", GUILayout.MaxWidth(30)) == true)
+            {
+                _variable.AsString = input;
+                input = "";
+            }
         }
     }
 
