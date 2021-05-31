500396f453c60322425e08bc052e7b33a2189fc9
LitQoo <litqoo@gmail.com>
2021-06-01 01:05:41 +0900

    [Feature] PageController.BackTo 추가

--- PageService/PageController.cs
+++ PageService/PageController.cs
@@ -287,6 +287,37 @@ namespace BicUtil.PageService
             }
         }
 
+        public void BackTo<PageClass>(PageTransition _transition, object _openParam = null){
+            var taskResult = this.BackToAsync<PageClass>(_transition, _openParam);
+            if(taskResult.Exception != null){
+                throw taskResult.Exception;
+            }
+        }
+
+        public async Task BackToAsync<PageClass>(PageTransition _transition, object _openParam = null){
+            Task _openTask = null; 
+            Task _closeTask = null;
+            while(true){
+                var _lastPage = pageStack.Pop();
+                _closeTask = _lastPage.OnClosedPage(CurrentPage, null);
+
+                if(this.CurrentPage is PageClass){
+                    if(_openParam != null){
+                        _openTask = this.CurrentPage.OnOpenedPage(_lastPage, _openParam);
+                    }
+
+                    break;
+                }
+
+                if(pageStack.Count == 1){
+                    Debug.LogError("[PageManager] BackToAsync is Fail");
+                    break;
+                }
+            }
+
+            await transitionPage(_transition, _closeTask, _openTask);
+        }
+
         internal async Task reopenFromSceneAsync(string _sceneName, Type _lastPageType, object _openParam){
             await CurrentPage.OnOpenedPage(new ScenePage(_sceneName, _lastPageType), _openParam);
         }
