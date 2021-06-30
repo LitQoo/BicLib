724133403457072195caab29805ac422f7b9d925
LitQoo <litqoo@gmail.com>
2021-07-01 00:08:50 +0900

    [Feature] TableCellCrossScrollSupporter 터치이벤트 감도 조절

--- TableView/TableCellCrossScrollSupporter.cs
+++ TableView/TableCellCrossScrollSupporter.cs
@@ -70,7 +70,7 @@ namespace BicUtil.TableView
         private void onEndDrag(PointerEventData _data){
             var _distance = Vector2.Distance(_data.position, startDragPosition);
 
-            if(_distance < 5f){
+            if(_distance < 15f){
                 onClickedCellAction.Invoke();
             }
 
