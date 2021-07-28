f898b9fff87a770a57fbef2a1483ad21d4669bd5
LitQoo <litqoo@gmail.com>
2021-07-29 05:05:32 +0900

    [Fix] Float.Tostring 도 문화권 상관없이 소수점으로 마침표사용하도록

--- DB/Variable/FloatVariable.cs
+++ DB/Variable/FloatVariable.cs
@@ -9,7 +9,7 @@ namespace BicDB.Variable
 		#region AsValue
 		protected virtual float data{ get; set;}
 		public int AsInt{ get{ return (int)AsFloat; } set{ AsFloat = value; } }
-		public string AsString{ get{ return AsFloat.ToString (); } set{ AsFloat = parse (value);} }
+		public string AsString{ get{ return AsFloat.ToString ("G", CultureInfo.InvariantCulture.NumberFormat); } set{ AsFloat = parse (value);} }
 		public float AsFloat{ get{ return data; } set{ data = value; NotifyChanged ();} }
 		public bool AsBool{ get{ return AsFloat == 0 ? false : true; } set{ AsFloat = (value ? 1 : 0) ;} }
 		public DataType Type { get { return DataType.Float; }}
