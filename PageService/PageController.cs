379c5341fdde1712588a877167ed364525a2611e
LitQoo <litqoo@gmail.com>
2022-09-23 16:04:17 +0900

    PageController BackTo, CloseTo 분리

--- PageService/PageController.cs
+++ PageService/PageController.cs
@@ -291,6 +291,32 @@ namespace BicUtil.PageService
                 _closeTask = _lastPage.OnClosedPage(CurrentPage, null);
 
                 if(this.CurrentPage is PageClass){
+                    _openTask = this.CurrentPage.OnOpenedPage(_lastPage, _openParam);
+
+                    break;
+                }
+
+                if(pageStack.Count == 1){
+                    Debug.LogError("[PageManager] BackToAsync is Fail");
+                    break;
+                }
+            }
+
+            await transitionPage(_transition, _closeTask, _openTask);
+        }
+
+        public async void CloseTo<PageClass>(PageTransition _transition, object _openParam = null){
+            await this.CloseToAsync<PageClass>(_transition, _openParam);
+        }
+
+        public async Task CloseToAsync<PageClass>(PageTransition _transition, object _openParam = null){
+            Task _openTask = null; 
+            Task _closeTask = null;
+            while(true){
+                var _lastPage = pageStack.Pop();
+                _closeTask = _lastPage.OnClosedPage(CurrentPage, null);
+
+                if(this.CurrentPage is PageClass){
                     if(_openParam != null){
                         _openTask = this.CurrentPage.OnOpenedPage(_lastPage, _openParam);
                     }
