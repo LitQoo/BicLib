c6e5d050ee9b0aadbbbff465f0e5bc04b1478481
LitQoo <litqoo@gmail.com>
2022-10-18 15:31:54 +0900

    fix prebuildprocess

--- GitVersions/Editor/PreBuildProcess.cs
+++ GitVersions/Editor/PreBuildProcess.cs
@@ -14,10 +14,14 @@ namespace BicLib.GitVersions{
         public void OnPreprocessBuild(BuildReport report)
         {
             var _gitStatus = GitUtil.Command("status");
-
+            
             if(_gitStatus.Contains("nothing to commit") == false){
-                Debug.LogError(_gitStatus);
-                throw new BuildFailedException("Can't build, commit first");
+                // if(_gitStatus.Contains("by 3 commit") == true && _gitStatus.Contains("UnityServicesProjectConfiguration.json") == true){
+                    Debug.Log("line " + _gitStatus.Split('\n').Length);
+                // }else{
+                    Debug.LogError(_gitStatus);
+                    throw new BuildFailedException("Can't build, commit first");
+                // }
             }
         }
     }
