55b027d1f627cf8cc8010d9830f1b5cd0ecef3c9
LitQoo <litqoo@gmail.com>
2022-10-18 14:44:30 +0900

    gitversions auto commit resource files

--- GitVersions/Editor/PostBuildProcess.cs
+++ GitVersions/Editor/PostBuildProcess.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 namespace BicLib.GitVersions{
     public class PostBuildProcess : MonoBehaviour
     {
-        [PostProcessBuild(0)]
+        [PostProcessBuild(200)]
         public static void PostProcessBuild(BuildTarget buildTarget, string _pathToBuiltProject){
+            var _gitStatus = GitUtil.Command("status");
+
+            if(_gitStatus.Contains("nothing to commit") == false){
+                GitUtil.Command("add .");
+                GitUtil.Command("commit -m \"update resource files auto\"");
+                Debug.Log("[BuildUtil] auto commit\n"+_gitStatus);    
+            }
+
             var _tag = buildTarget.ToString()+"_"+BicLib.BuildUtil.BuildUtil.GetBuildString();
             GitUtil.Command("tag " + _tag);
             Debug.Log("[BuildUtil] git tag " + _tag);
