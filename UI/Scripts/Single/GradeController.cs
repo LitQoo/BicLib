324932a9c6ce2aeb5c1a2a4d6ab9e9289c3695b2
LitQoo <litqoo@gmail.com>
2023-01-02 15:14:39 +0900

    implement GradeController.Effector

--- UI/Scripts/Single/GradeController.cs
+++ UI/Scripts/Single/GradeController.cs
@@ -17,62 +17,124 @@ namespace BicUtil.UI{
         #endregion
 
         #region Data
+        public delegate void Effector(MarkType _markType, GameObject _markObject, bool _isActive, bool _isInitial); 
         public IVariableReadOnly Grade {get=>this.grade;}
         public IVariableReadOnly GradeLife {get=>this.gradeLife;}
+        
         public bool IsPerfact{
             get{
                 return this.grade.AsInt == this.gradeLifeMax.Length 
                         && this.gradeLife.AsInt == this.gradeLifeMax[this.gradeLifeMax.Length - 1];
             }
         }
-
+        
+        private Effector effector = null;
         private EncryptedIntVariable grade = new EncryptedIntVariable(1);
         private EncryptedIntVariable gradeLife = new EncryptedIntVariable(0);  
         private int[] gradeLifeMax;
         private bool isSubscribe = false;
+
+        public enum MarkType{
+            Grade,
+            Life,
+            Perfact
+        }
         #endregion
 
         #region Logic
-        private void updateGrade(IVariableReadOnly _grade)
-        {
-            for(int i = 0;i < gradeObjects.Length; i++){
-                    gradeObjects[i].SetActive(_grade.AsInt > i);
+        public void Setup(int[] _gradeLifeMax, Effector _effector = null){
+            this.effector = _effector;
+            this.gradeLifeMax = _gradeLifeMax;
+            this.grade.AsIntWithoutNotify = this.gradeLifeMax.Length;
+            
+            if(isSubscribe == false){
+                isSubscribe = true;
+                grade.Subscribe(updateGrade);
+                gradeLife.Subscribe(updateGradeLife);
             }
 
-            this.gradeLife.AsInt = this.gradeLifeMax[this.grade.AsInt - 1];
-
+            setupGrade(this.grade, true);
+            setupGradeLife(this.gradeLife, true);
         }
 
-        private void updatePerfact()
+
+        private void setupGrade(IVariableReadOnly _grade, bool _isInitial)
+        {
+            for(int i = 0;i < gradeObjects.Length; i++){
+                changeGrade(gradeObjects[i], _grade.AsInt > i, _isInitial);
+            }
+
+            if(_isInitial == false){
+                this.gradeLife.AsInt = this.gradeLifeMax[this.grade.AsInt - 1];
+            }else{
+                this.gradeLife.AsIntWithoutNotify = this.gradeLifeMax[this.grade.AsInt - 1];
+            }
+        }
+
+        private void updateGrade(IVariableReadOnly _grade)
+        {
+            setupGrade(_grade, false);
+        }
+
+        private void changeGrade(GameObject _mark, bool _isActive, bool _isInitial){
+            if(_mark.activeSelf != _isActive || _isInitial == true){
+                if(effector == null){
+                    active(_mark, _isActive);
+                }else{
+                    effector(MarkType.Grade, _mark, _isActive, _isInitial);
+                }
+            }
+        }
+
+        private void active(GameObject _mark, bool _isActive){
+            _mark.SetActive(_isActive);
+        }
+
+        private void updatePerfact(bool _isInitial)
         {
             for(int i = 0;i < perfactObjects.Length; i++){
-                    perfactObjects[i].SetActive(this.IsPerfact);
+                changePerfact(perfactObjects[i], this.IsPerfact, _isInitial);
             }
+        }
+
+        private void changePerfact(GameObject _mark, bool _isActive, bool _isInitial){
+            if(_mark.activeSelf != _isActive || _isInitial == true){
+                if(effector == null){
+                    active(_mark, _isActive);
+                }else{
+                    effector(MarkType.Perfact, _mark, _isActive, _isInitial);
+                }
+            }
+        }
+
+        private void setupGradeLife(IVariableReadOnly _gradeLife, bool _isInitial)
+        {
+            for(int i = 0;i < gradeLifeObjects.Length; i++){
+                changeGradeLife(gradeLifeObjects[i], _gradeLife.AsInt > i, _isInitial);
+            }
+
+            updatePerfact(_isInitial);
         }
 
         private void updateGradeLife(IVariableReadOnly _gradeLife)
         {
-            for(int i = 0;i < gradeLifeObjects.Length; i++){
-                    gradeLifeObjects[i].SetActive(_gradeLife.AsInt > i);
-            }
-
-            updatePerfact();
+            setupGradeLife(_gradeLife, false);
         }
 
-        public void Setup(int[] _gradeLifeMax){
-            this.gradeLifeMax = _gradeLifeMax;
-            this.grade.AsInt = this.gradeLifeMax.Length;
 
-            if(isSubscribe == false){
-                isSubscribe = true;
-                grade.Subscribe(updateGrade, true);
-                gradeLife.Subscribe(updateGradeLife, true);
+        private void changeGradeLife(GameObject _mark, bool _isActive, bool _isInitial){
+            if(_mark.activeSelf != _isActive || _isInitial == true){
+                if(effector == null){
+                    active(_mark, _isActive);
+                }else{
+                    effector(MarkType.Life, _mark, _isActive, _isInitial);
+                }
             }
         }
 
         public void DecreaseGradeLife(){
             this.gradeLife.AsInt--;
-            if(this.gradeLife.AsInt == 0){
+            if(this.gradeLife.AsInt <= 0 && IsPerfact == false){
                 this.grade.AsInt = Mathf.Clamp(this.grade.AsInt - 1, 1, this.gradeLifeMax.Length);
             }
         }
