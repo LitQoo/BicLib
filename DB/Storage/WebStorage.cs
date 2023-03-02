d7a0896dcd6414cd19d8a3d26aa664cba53e1205
LitQoo <litqoo@gmail.com>
2023-03-02 14:36:21 +0900

    fix WebStorage 리소스 캐싱 순서 조절

--- DB/Storage/WebStorage.cs
+++ DB/Storage/WebStorage.cs
@@ -615,13 +615,6 @@ namespace BicDB.Storage
 				return (CachingLevel.None, string.Empty);
 			}
 
-			if(_param.ResourceCacheEnableBeforeWeb == true){
-				string _result = loadCacheFromResrouceAndCaching(_param);
-				if(string.IsNullOrEmpty(_result) == false){
-					return (CachingLevel.Resource, _result);
-				}
-			}
-
 			//캐시타임 체크해서 지났으면 캐시로드하지 말고 그냥로드 캐시타임은 메모리, 파일 따로 조사해야할듯? 그럼 파일 json파싱을 해야하는데? -0-
 			if((_param.CachingLevel & CachingLevel.Memory) != 0){
 				lock(memoryCache){
@@ -634,6 +627,19 @@ namespace BicDB.Storage
 							//Debug.Log("cache timeout");
 						}
 					}
+				}
+			}
+
+			if(_param.ResourceCacheEnableBeforeWeb == true){
+				string _result = loadCacheFromResrouceAndCaching(_param);
+				if(string.IsNullOrEmpty(_result) == false){
+					if((_param.CachingLevel & CachingLevel.Memory) != 0){
+						lock(memoryCache){
+							memoryCache[_param.CacheId] = (getTimestamp(), _result);
+						}
+					}
+
+					return (CachingLevel.Resource, _result);
 				}
 			}
 			
