d09e5dfcf14e7730ab604a235f90a5fa23dfe3aa
LitQoo <litqoo@gmail.com>
2023-05-17 13:23:56 +0900

    Extensions.Shuffle 개선

--- CSharpExtensions/Extensions.cs
+++ CSharpExtensions/Extensions.cs
@@ -18,7 +18,7 @@ namespace BicUtil.CSharpExtensions
     {
         private static System.Random rng = new System.Random();  
 
-        public static void Shuffle<T>(this IList<T> list)  
+        public static IList<T> Shuffle<T>(this IList<T> list)  
         {  
             int n = list.Count;  
             while (n > 1) {  
@@ -28,6 +28,22 @@ namespace BicUtil.CSharpExtensions
                 list[k] = list[n];  
                 list[n] = value;  
             }  
+
+            return list;
+        }
+
+        public static List<T> Shuffle<T>(this List<T> list)  
+        {  
+            int n = list.Count;  
+            while (n > 1) {  
+                n--;  
+                int k = rng.Next(n + 1);  
+                T value = list[k];  
+                list[k] = list[n];  
+                list[n] = value;  
+            }  
+
+            return list;
         }
 
         public static Vector2Int ConvertToVector2Int(this Vector3 _vector){
