fdaa074c7a3e45c99fb379f572a8414787ee8ce5
LitQoo <litqoo@gmail.com>
2023-07-05 23:28:59 +0900

    Teacher DottedLine 추가

---
+++ Teacher/DottedLineController.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BicUtil.Teacher{
+    [RequireComponent(typeof(LineRenderer))]
+    [ExecuteInEditMode]
+    public class DottedLineController : MonoBehaviour
+    {
+        #region DI
+        [SerializeField]
+        private LineRenderer lineRenderer;
+        [SerializeField]
+        private SpriteRenderer head;
+        [SerializeField]
+        private Shader shader;
+        #endregion
+
+        private void Awake(){
+            this.lineRenderer.material = new Material(this.shader);
+        }
+
+        public void SetPosition(Vector3[] _positions, float _startOffset = 0f, float _endOffset = 0f, bool _reload = true){
+            lineRenderer.positionCount = _positions.Length;
+            
+            if(_startOffset > 0){
+                var _distance = Vector3.Distance(_positions[1], _positions[0]);
+                var _offsetPosition = Vector3.Lerp(_positions[1], _positions[0], (_distance - _startOffset) / _distance);
+                _positions[0] = _offsetPosition;
+            }
+
+            if(_endOffset > 0){
+                var _count = _positions.Length;
+                var _distance = Vector3.Distance(_positions[_count - 2], _positions[_count - 1]);
+                var _offsetPosition = Vector3.Lerp(_positions[_count - 2], _positions[_count - 1], (_distance - _endOffset) / _distance);
+                _positions[_count - 1] = _offsetPosition;
+            }
+
+            lineRenderer.SetPositions(_positions);
+
+            if(_reload == true){
+                reloadDot();
+            }
+        }
+
+        public void SetSpacing(float _space, bool _reload = true){
+            lineRenderer.material.SetFloat("_Spacing", _space);
+            if(_reload == true){
+                reloadDot();
+            }
+        }
+
+        public void SetSpeed(float _speed){
+            lineRenderer.material.SetFloat("_Speed", _speed);
+        }
+
+        public void SetDotSize(float _size, bool _reload = true){
+            lineRenderer.endWidth = _size;
+            lineRenderer.startWidth = _size;
+            if(_reload == true){
+                reloadDot();
+            }
+        }
+
+        public void SetColor(Color _color){
+            lineRenderer.endColor = _color;
+            lineRenderer.startColor = _color;
+
+            if(head != null){
+                head.color = _color;
+            }
+        }
+
+        private void reloadDot()
+        {
+            var _dotR = lineRenderer.startWidth;
+            var _distance = 0f;
+            var _positionCount = lineRenderer.positionCount;
+            for(int i = 1; i < _positionCount; i++){
+                _distance += Vector2.Distance(lineRenderer.GetPosition(i), lineRenderer.GetPosition(i - 1));
+            }
+
+            var _space = lineRenderer.material.GetFloat("_Spacing");
+            var _repeatCount = _distance / (_dotR + _dotR * _space);  
+            lineRenderer.material.SetFloat("_RepeatCount", _repeatCount);
+
+            if(head != null){
+                head.transform.localPosition = lineRenderer.GetPosition(_positionCount - 1);
+                Vector2 direction = lineRenderer.GetPosition(_positionCount - 1) - lineRenderer.GetPosition(_positionCount - 2);
+                float _angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                head.transform.rotation = Quaternion.Euler(0, 0, _angle);
+            }
+        }
+
+        // #if UNITY_EDITOR
+        // private void Update(){
+        //    reloadDot();
+        // }
+        // #endif
+    }
+}
---
+++ Teacher/Teacher.cs
@@ -0,0 +1,394 @@
+using System;
+using BicUtil.Tween;
+using UnityEngine;
+
+namespace BicUtil.Teacher
+{
+    public class Teacher{
+        private TeacherController controller;
+        
+        public Teacher(TeacherController _controller){
+            this.controller = _controller;
+        }
+
+
+        public void Spawn(Action _scenario){
+            controller.spawn(_scenario);
+        }
+
+        public void WaitClickedButton(string _buttonText){
+            SetButton(_buttonText);
+            BicTween.WaitInputData().AddTo(controller.parentTween);
+            HideButton();
+        }
+
+        public void Wait(Func<bool> _updateFunc){
+            var _tween = BicTween.WaitInputData().AddTo(controller.parentTween);
+            _tween.SubscribeUpdate(_update=>{
+                if(_updateFunc() == true){
+                    _tween.Data = "Next";
+                }
+            });
+        }
+
+        private Vector2 touchBeganPosition;
+        public void SetPointerWithScaleUp(TeachPointerType _type){
+            this.SetPointerWithTween(_type, _pointer=>{
+                return BicTween.Scale(_pointer, Vector2.zero, Vector2.one, 0.2f).SubscribeStart(()=>{
+                    _pointer.transform.localScale = Vector2.zero;
+                }).SetEase(EaseType.OutBack);
+            });
+        }
+
+
+        public void SetButton(string _title){
+            BicTween.Delay(0f).SubscribeStart(()=>
+            {
+                controller.button.gameObject.SetActive(true);
+                controller.buttonText.text = _title;
+            }).AddTo(controller.parentTween);
+        }
+
+        public void HideButton(){
+            BicTween.Delay(0f).SubscribeStart(()=>
+            {
+                controller.button.gameObject.SetActive(false);
+            }).AddTo(controller.parentTween);
+        }
+
+        public void SetTitle(string _title, Color _color){
+            BicTween.Delay(0f).SubscribeStart(()=>
+            {
+                controller.title.gameObject.SetActive(true);
+                controller.title.text = _title;
+                controller.title.color = _color;
+            }).AddTo(controller.parentTween);
+        }
+
+        public void SetDiscription(string _message, Color _color){
+            BicTween.Delay(0f).SubscribeStart(()=>
+            {
+                controller.discription.gameObject.SetActive(true);
+                controller.discription.text = _message;
+                controller.discription.color = _color;
+            }).AddTo(controller.parentTween);
+        }
+
+        public void SetDiscriptionScaleUp(string _message, Color _color){
+            
+            BicTween.Scale(controller.discription.gameObject, Vector2.one * 0.5f, Vector2.one, 0.5f).SubscribeStart(()=>{
+                controller.discription.gameObject.SetActive(true);
+                controller.discription.text = _message;
+                controller.discription.color = _color;
+                controller.discription.transform.localScale = Vector2.one * 0.5f;
+            }).SetEase(EaseType.OutBack).AddTo(controller.parentTween);
+        }
+
+        public void SetDiscriptionTypeWriter(string _message, Color _color){
+            BicTween.TypeWriter(controller.discription, _message, 0.07f).SubscribeStart(()=>
+            {
+                controller.discription.gameObject.SetActive(true);
+                controller.discription.text = "";
+                controller.discription.color = _color;
+            }).AddTo(controller.parentTween);
+        }
+
+        public void ClearTitleAndDiscription(){
+            BicTween.Delay(0f).SubscribeStart(()=>
+            {
+                controller.title.gameObject.SetActive(false);
+                controller.title.text = string.Empty;
+                controller.discription.gameObject.SetActive(false);
+                controller.discription.text = string.Empty;
+            }).AddTo(controller.parentTween);
+        }
+
+        public void ClearTitle(){
+            BicTween.Delay(0f).SubscribeStart(()=>
+            {
+                controller.title.gameObject.SetActive(false);
+                controller.title.text = string.Empty;
+            }).AddTo(controller.parentTween);
+        }
+
+        public void ClearDiscription(){
+
+            BicTween.Delay(0f).SubscribeStart(()=>
+            {
+                controller.discription.gameObject.SetActive(false);
+                controller.discription.text = string.Empty;
+            }).AddTo(controller.parentTween);
+        }
+
+        public void HidePointerWithScaleDown(TeachPointerType _type){
+            this.SetPointerWithTween(_type, _pointer=>{
+                var _tween = BicTween.Scale(_pointer, Vector2.one, Vector2.zero, 0.2f);
+                _tween.SubscribeStart(()=>{
+                    _tween.OriginValue = _pointer.transform.localScale;
+                    _tween.DiffValue = Vector2.zero - (Vector2)_tween.OriginValue;
+                }).SetEase(EaseType.OutQuad).SubscribeComplete(()=>{
+                    _pointer.transform.localScale = Vector2.one;
+                    _pointer.gameObject.SetActive(false);
+                });
+                return _tween;
+            });
+        }
+
+        public void SetPointerWithTween(TeachPointerType _type, Func<GameObject, Tween.Tween> _func){
+            var _tween = _func(this.controller.pointer.gameObject);
+            _tween.SubscribeStart(()=>{
+                setPointer(_type);
+            }).AddTo(controller.parentTween);
+        }
+
+        public void DoubleTapWithSimulate(){
+            this.SetPointer(TeachPointerType.OneDownWithSimulate);
+            this.Delay(0.05f);
+            this.SetPointer(TeachPointerType.OneUpWithSimulate);
+            this.Delay(0.05f);
+            this.SetPointer(TeachPointerType.OneDownWithSimulate);
+            this.Delay(0.05f);
+            this.SetPointer(TeachPointerType.OneUpWithSimulate);
+        }
+
+        public void SingleTapWithSimulate(){
+            this.SetPointer(TeachPointerType.OneDownWithSimulate);
+            this.Delay(0.05f);
+            this.SetPointer(TeachPointerType.OneUpWithSimulate);
+        }
+
+        public void SetPointer(TeachPointerType _type){
+            BicTween.Delay(0f).SubscribeStart(()=>
+            {
+                setPointer(_type);
+            }).AddTo(controller.parentTween);
+        }
+
+        private void setPointer(TeachPointerType _type)
+        {
+            controller.pointer.gameObject.SetActive(true);
+            switch (_type)
+            {
+                case TeachPointerType.None: controller.pointer.gameObject.SetActive(false); break;
+                case TeachPointerType.One:
+                    controller.pointer.sprite = controller.pointerOne;
+                    break;
+                case TeachPointerType.OneUp:
+                case TeachPointerType.OneUpWithSimulate:
+                    controller.pointer.sprite = controller.pointerOne;
+                    if (controller.touchModule != null &&  _type == TeachPointerType.OneUpWithSimulate)
+                    {
+                        controller.touchModule.OnTouchUp.Invoke(controller.pointer.transform.position, controller.pointer.transform.position, 0);
+                    }
+
+                    break;
+                case TeachPointerType.OneDown:
+                case TeachPointerType.OneDownWithSimulate:
+                    controller.pointer.sprite = controller.pointerOneDown;
+                    if (controller.touchModule != null &&  _type == TeachPointerType.OneDownWithSimulate)
+                    {
+                        touchBeganPosition = controller.pointer.transform.position;
+                        controller.touchModule.OnTouchDown.Invoke(controller.pointer.transform.position, 0);
+                    }
+                    break;
+                case TeachPointerType.Two: controller.pointer.sprite = controller.pointerTwo; break;
+                case TeachPointerType.ZoomIn: controller.pointer.sprite = controller.pointerZoomIn; break;
+                case TeachPointerType.ZoomOut: controller.pointer.sprite = controller.pointerZoomOut; break;
+                case TeachPointerType.ZoomReady: controller.pointer.sprite = controller.pointerZoomReady; break;
+            }
+        }
+
+        public void SetPosition(Vector2 _position){
+            BicTween.Delay(0f).SubscribeStart(()=>{
+                controller.pointer.gameObject.transform.position = _position;
+            }).AddTo(controller.parentTween);
+        }
+
+        public void SetPositionFollow(GameObject _target){
+            BicTween.Delay(0f).SubscribeStart(()=>{
+                controller.pointer.gameObject.transform.position = _target.transform.position;
+            }).AddTo(controller.parentTween);
+        }
+
+        public void ZoomWithSimulate(float _size, float _time){
+            var _firstTouchSize = 30f*Vector2.one;
+            BicTween.Value(0, _size, _time).SubscribeStart(()=>
+            {
+                if(_size > 0){
+                    setPointer(TeachPointerType.ZoomIn);
+                }else{
+                    setPointer(TeachPointerType.ZoomOut);
+                }
+                touchBeganPosition = controller.pointer.transform.position;
+            
+                controller.touchModule.GetTouchReturnForced = true;
+                controller.touchModule.EnableUpdate = false;
+                controller.touchModule.SetTouchCountForced(2);
+                controller.touchModule.SetStartPositionForced(0, touchBeganPosition + _firstTouchSize);
+                controller.touchModule.OnTouchDown.Invoke(touchBeganPosition + _firstTouchSize, 0);
+                controller.touchModule.OnTouchDown.Invoke(touchBeganPosition - _firstTouchSize, 1);
+            }).SubscribeUpdate(_value=>{
+                if(controller.touchModule != null){
+                    controller.touchModule.SetTouchCountForced(2);
+                    controller.touchModule.SetStartPositionForced(0, touchBeganPosition + _firstTouchSize);
+                    controller.touchModule.OnTouchMove.Invoke(touchBeganPosition + _firstTouchSize, touchBeganPosition + _firstTouchSize + Vector2.one*_value.x, 0);
+                    controller.touchModule.OnTouchMove.Invoke(touchBeganPosition - _firstTouchSize, touchBeganPosition - _firstTouchSize - Vector2.one*_value.x, 1);
+                }
+            }).SubscribeComplete(()=>{
+                setPointer(TeachPointerType.ZoomReady);
+                controller.touchModule.SetTouchCountForced(2);
+                controller.touchModule.OnTouchUp.Invoke(touchBeganPosition + _firstTouchSize, touchBeganPosition + _firstTouchSize + Vector2.one * _size, 0);
+                controller.touchModule.OnTouchUp.Invoke(touchBeganPosition - _firstTouchSize, touchBeganPosition - _firstTouchSize - Vector2.one * _size, 1);
+                controller.touchModule.SetTouchCountForced(0);
+                controller.touchModule.GetTouchReturnForced = false;
+                controller.touchModule.EnableUpdate = true;
+            }).AddTo(controller.parentTween);
+
+            
+        }
+
+        public void MovePointerTo(Vector2 _position, float _time, bool _simulateTouch){
+            var _tween = BicTween.MoveWorld(controller.pointer.gameObject, _position, _time);
+
+            _tween.SubscribeStart(()=>{
+                _tween.OriginValue = this.controller.pointer.transform.position;
+                _tween.DiffValue = _position - (Vector2)_tween.OriginValue; 
+            }).SubscribeUpdate(_position=>{
+                if(controller.touchModule != null && _simulateTouch == true){
+                    controller.touchModule.OnTouchMove.Invoke(touchBeganPosition, _position, 0);
+                }
+            }).AddTo(controller.parentTween);
+        }
+
+        public void MovePointerFollow(GameObject _target, float _time){
+            
+            var _tween = BicTween.MoveWorld(controller.pointer.gameObject, _target.transform.position, _time);
+            
+            _tween.SubscribeStart(()=>{
+                _tween.OriginValue = this.controller.pointer.transform.position;
+                _tween.DiffValue = _target.transform.position - (Vector3)_tween.OriginValue; 
+            }).AddTo(controller.parentTween);
+        }
+
+        public void MovePointerFollow(GameObject _target, Vector2 _offset, float _time){
+            var _tween = BicTween.MoveWorld(controller.pointer.gameObject, _target.transform.position, _time);
+
+            _tween.SubscribeStart(()=>{
+                _tween.OriginValue = this.controller.pointer.transform.position;
+                _tween.DiffValue = _target.transform.position + (Vector3)_offset - (Vector3)_tween.OriginValue; 
+            }).AddTo(controller.parentTween);
+        }
+
+        public void Delay(float _time){
+            BicTween.Delay(_time).AddTo(controller.parentTween);
+        }
+
+        public void ClickButton(UnityEngine.UI.Button _button){
+            throw new NotFiniteNumberException();
+            //ExecuteEvents.Execute(button.gameObject, new BaseEventData(eventSystem), ExecuteEvents.submitHandler);
+        }
+
+        public void SetDimmed(bool _isActive, Color _color, float _time){
+            var _tween = BicTween.Value((Vector4)controller.dimmed.color, (Vector4)_color, _time);
+            _tween.SubscribeStart(()=>{
+                if(_isActive == true){
+                    if(controller.dimmed.gameObject.activeSelf == false){
+                        controller.dimmed.color = Color.clear;
+                    }
+
+                    controller.dimmed.gameObject.SetActive(true);
+                }
+
+                _tween.OriginValue = (Vector4)controller.dimmed.color;
+                _tween.DiffValue = (Vector4)_color - _tween.OriginValue;
+            }).SubscribeUpdate(_value=>{
+                controller.dimmed.color = _value;
+            }).SubscribeComplete(()=>{
+                controller.dimmed.gameObject.SetActive(_isActive);
+            }).AddTo(controller.parentTween);
+        }
+
+        public void SetSpotlight(int _index, Vector2 _position, Vector2 _size){
+            BicTween.Delay(0).SubscribeStart(()=>
+            {
+                setSpotlight(_index, _position, _size);
+            }).AddTo(controller.parentTween);
+
+        }
+
+        public void SetSpotlightWithScaleUp(int _index, Vector2 _position, Vector2 _size){
+            BicTween.Scale(controller.spotlights[_index].gameObject, Vector2.zero, Vector2.one, 0.2f).SubscribeStart(()=>{
+                controller.spotlights[_index].transform.localScale = Vector2.zero;
+                controller.spotlights[_index].gameObject.SetActive(true);
+                setSpotlight(_index, _position, _size);
+            }).AddTo(controller.parentTween).SetEase(EaseType.OutBack);
+        }
+
+        public void HideSpotlightWithScaleDown(int _index){
+            BicTween.Scale(controller.spotlights[_index].gameObject, Vector2.one, Vector2.zero, 0.2f).SubscribeStart(()=>{
+                controller.spotlights[_index].transform.localScale = Vector2.one;
+                controller.spotlights[_index].gameObject.SetActive(true);
+            }).AddTo(controller.parentTween).SetEase(EaseType.OutBack).SubscribeComplete(()=>{
+                controller.spotlights[_index].transform.localScale = Vector2.one;
+                controller.spotlights[_index].gameObject.SetActive(false);
+            });
+        }
+
+        private void setSpotlight(int _index, Vector2 _position, Vector2 _size)
+        {
+            controller.spotlights[_index].gameObject.SetActive(true);
+            controller.spotlights[_index].transform.position = _position;
+            controller.spotlights[_index].rectTransform.sizeDelta = _size;
+        }
+
+        public void MoveSpotlight(int _index, Vector2 _position, float _time){
+            var _spot = controller.spotlights[_index];
+            var _tween = BicTween.MoveWorld(_spot.gameObject, _position, _time);
+
+            _tween.SubscribeStart(()=>{
+                _tween.OriginValue = _spot.transform.position;
+                _tween.DiffValue = _position - (Vector2)_tween.OriginValue; 
+            }).AddTo(controller.parentTween);
+        }
+
+        public void SetDottedLine(int _index, Vector2 _positionStart, Vector2 _positionEnd, Color _color, float _width, float _space, float _speed = 0f, float _startOffset = 0f, float _endOffset = 0f){
+            var _line = controller.dottedLines[_index];
+            BicTween.Delay(0).SubscribeStart(()=>
+            {
+                _line.SetColor(_color);
+                _line.SetPosition(new Vector3[]{_positionStart, _positionEnd}, _startOffset, _endOffset, false);
+                _line.SetDotSize(_width); 
+                _line.SetSpeed(_speed);
+                _line.SetSpacing(_space);
+                _line.gameObject.SetActive(true);
+            }).AddTo(controller.parentTween);
+        }
+
+        public void HideDottedLine(int _index){
+            BicTween.Delay(0).SubscribeStart(()=>
+            {
+                var _line = controller.dottedLines[_index];
+                _line.gameObject.SetActive(false);
+            }).AddTo(controller.parentTween);
+        }
+
+        public void Action(Action _action){
+            var _tween = BicTween.Delay(0f);
+            _tween.SubscribeStart(_action).AddTo(controller.parentTween);
+        }
+
+    }
+
+    public enum TeachPointerType{
+        None,        
+        One,
+        OneDown,
+        OneUp,
+        OneDownWithSimulate,
+        OneUpWithSimulate,
+        Two,
+        ZoomIn,
+        ZoomOut,
+        ZoomReady
+    }
+}
