18f73540346b862d0f2261f43ae7417430e98c61
LitQoo <litqoo@gmail.com>
2024-07-12 13:42:05 +0900

    빌드시 커밋메세지 입력 기능 추가

--- GitVersions/Editor/PostBuildProcess.cs
+++ GitVersions/Editor/PostBuildProcess.cs
@@ -7,17 +7,22 @@ namespace BicLib.GitVersions{
     {
         [PostProcessBuild(200)]
         public static void PostProcessBuild(BuildTarget buildTarget, string _pathToBuiltProject){
+            
             var _gitStatus = GitUtil.Command("status");
 
             if(_gitStatus.Contains("nothing to commit") == false){
-                GitUtil.Command("add .");
-                GitUtil.Command("commit -m \"update resource files auto\"");
-                Debug.Log("[BuildUtil] auto commit\n"+_gitStatus);    
+                if(PreBuildProcess.SouldCommit == true){
+                    GitUtil.Command("add .");
+                    GitUtil.Command("commit -m \""+PreBuildProcess.CommitMessage+"\"");
+                    Debug.Log("[BuildUtil] auto commit\n"+_gitStatus);
+                }    
             }
 
-            var _tag = buildTarget.ToString()+"_"+BicLib.BuildUtil.BuildUtil.GetBuildString();
-            GitUtil.Command("tag " + _tag);
-            Debug.Log("[BuildUtil] git tag " + _tag);
+            if(_gitStatus.Contains("nothing to commit") == true || PreBuildProcess.SouldCommit == true){
+                var _tag = buildTarget.ToString()+"_"+BicLib.BuildUtil.BuildUtil.GetBuildString();
+                GitUtil.Command("tag " + _tag);
+                Debug.Log("[BuildUtil] git tag " + _tag);
+            }
         }
     }
 }
--- GitVersions/Editor/PreBuildProcess.cs
+++ GitVersions/Editor/PreBuildProcess.cs
@@ -10,26 +10,36 @@ namespace BicLib.GitVersions{
     public class PreBuildProcess : IPreprocessBuildWithReport
     {
         public int callbackOrder => 0;
-
+        static public bool SouldCommit = false; 
+        static public string CommitMessage = "update resource files auto";
         public void OnPreprocessBuild(BuildReport report)
         {
             var _gitStatus = GitUtil.Command("status");
-
+            SouldCommit = false;
+            CommitMessage = "update resource files auto";
             if(_gitStatus.Contains("nothing to commit") == false){
-                if(_gitStatus.Contains("Changes not staged for commit") == true){
-                    if(EditorUtility.DisplayDialog("Git warning", "커밋하지 않은 내용이 있습니다.\n계속하시겠습니까?\n(빌드후 자동 커밋됩니다.)", "확인", "취소") == false){
+                if(_gitStatus.Contains("Changes not staged for commit") == true || (_gitStatus.Split('\n').Length <= 11 && _gitStatus.Contains("nothing added to commit but untracked files present") == true && _gitStatus.Contains("UnityServicesProjectConfiguration.json") == true) == false){
+                    int option = EditorUtility.DisplayDialogComplex(
+                        "Git warning",                 // 제목
+                        "커밋하지 않은 내용이 있습니다.", // 메시지
+                        "커밋 후 빌드",                          // 첫 번째 버튼 텍스트
+                        "커밋하지 않고 빌드",                          // 두 번째 버튼 텍스트
+                        "취소"                           // 세 번째 버튼 텍스트
+                    );
+                    
+                    if(option == 2){
                         Debug.LogError(_gitStatus);
                         throw new BuildFailedException("Can't build, commit first");
+                    }else if(option == 0){
+                        SouldCommit = true;
+                        
+                        var name = EditorCommitDialog.Show( "Commit", _gitStatus, "update resource files auto");
+                        if(string.IsNullOrEmpty(name) == true){
+                          throw new BuildFailedException("Cancel build");
+                        }
+
+                        CommitMessage = name;
                     }
-
-                    return;
-                }
-
-                if(_gitStatus.Split('\n').Length <= 11 && _gitStatus.Contains("nothing added to commit but untracked files present") == true && _gitStatus.Contains("UnityServicesProjectConfiguration.json") == true){
-                    
-                }else if(EditorUtility.DisplayDialog("Git warning", "커밋하지 않은 내용이 있습니다.\n계속하시겠습니까?\n(빌드후 자동 커밋됩니다.)", "확인", "취소") == false){
-                    Debug.LogError(_gitStatus);
-                    throw new BuildFailedException("Can't build, commit first");
                 }
             }
         }
